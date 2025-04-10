6051f014a19fc38799352ed3ef4af359fb12ee20
Raj <patel.raj.7021@gmail.com>
2025-04-10 00:18:14 -0400

    add core sorting code - need test cases still

--- src/modified_counting_sort.py
+++ src/modified_counting_sort.py
@@ -1,3 +1,18 @@
-def sort(list_in):
-    
-    return
+from collections import defaultdict
+
+def mod_count_sort(list_in, sorting_index):    
+    # assuming list_in is an iterable of iterables
+    # e.g., of the format
+    # [ (sorting_value, other_value, ...), ...]
+    # sorting_index indicates which index of the inner iterable to sort by
+    indices = defaultdict(list)
+    for i, elem in enumerate(list_in):
+        indices[elem[sorting_index]].append(i)
+    list_out = []
+    keys = indices.keys()
+    keys.sort(reverse=True)
+    for key in keys:
+        target_indices = indices[key]
+        for target_index in target_indices:
+            list_out.append(list_in[target_index])
+    return list_out
